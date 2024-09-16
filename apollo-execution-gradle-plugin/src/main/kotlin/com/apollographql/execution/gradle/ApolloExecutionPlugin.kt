f2800131be4a64b151c0abc6d75c9c90eb2770de
Martin Bonnin <martin@mbonnin.net>
2024-09-16 12:00:41 +0200

    Uniformize integrations and add automatic version resolution (#28)
    * Uniformize integrations
    
    * update apiDump
    
    * automatically resolve the version
    
    * Also build the samples in PRs

--- apollo-execution-gradle-plugin/src/main/kotlin/com/apollographql/execution/gradle/ApolloExecutionPlugin.kt
+++ apollo-execution-gradle-plugin/src/main/kotlin/com/apollographql/execution/gradle/ApolloExecutionPlugin.kt
@@ -2,9 +2,22 @@ package com.apollographql.execution.gradle
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.ExternalDependency
 
 abstract class ApolloExecutionPlugin : Plugin<Project> {
   override fun apply(target: Project) {
     target.extensions.create("apolloExecution", ApolloExecutionExtension::class.java, target)
+    target.configureDefaultVersionsResolutionStrategy()
+  }
+
+  private fun Project.configureDefaultVersionsResolutionStrategy() {
+    configurations.configureEach { configuration ->
+      configuration.withDependencies { dependencySet ->
+        val pluginVersion = VERSION
+        dependencySet.filterIsInstance<ExternalDependency>()
+          .filter { it.group == "com.apollographql.execution" && it.version.isNullOrEmpty() }
+          .forEach { it.version { constraint -> constraint.require(pluginVersion) } }
+      }
+    }
   }
 }
