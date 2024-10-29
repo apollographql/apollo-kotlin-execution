fe5d72789d5f897698186ebf1bc3a32d6ffc68fb
Martin Bonnin <martin@mbonnin.net>
2024-10-29 10:59:03 +0100

    Unbreak docs publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,8 @@ buildscript {
         classpath(libs.kotlin.gradle.plugin)
         classpath(libs.librarian.gradle.plugin)
         classpath(libs.wire.gradle.plugin)
+        // workaround for https://github.com/Kotlin/kotlinx.serialization/issues/2803
+        classpath(libs.kotlinx.serialization.core)
     }
     repositories {
         mavenCentral()
