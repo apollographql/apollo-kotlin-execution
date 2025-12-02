cddd6c38ef40427ce9fe29316620863c1ba6e323
Martin Bonnin <martin@mbonnin.net>
2025-12-02 15:20:59 +0100

    Add KDoc

--- apollo-execution-runtime/src/commonMain/kotlin/com/apollographql/execution/annotation/GraphQLDefault.kt
+++ apollo-execution-runtime/src/commonMain/kotlin/com/apollographql/execution/annotation/GraphQLDefault.kt
@@ -1,4 +1,9 @@
 package com.apollographql.execution.annotation
 
+/**
+ * KSP cannot access the default values of functions. Use [com.apollographql.execution.annotation.GraphQLDefault] to provide them.
+ *
+ * @param value the GraphQL default value as a [GraphQL value literal](https://spec.graphql.org/draft/#Value).
+ */
 @Target(AnnotationTarget.VALUE_PARAMETER)
 annotation class GraphQLDefault(val value: String)
